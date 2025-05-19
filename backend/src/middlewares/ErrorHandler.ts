3b0ef7b815973afa48de7082b7ebac44fa3c48cb
Renato Guedes <105191060+renatoguedes-dev@users.noreply.github.com>
2025-05-19 15:32:04 -0300

    feat: include upload route and general error handler in the server config

---
+++ backend/src/middlewares/ErrorHandler.ts
@@ -0,0 +1,34 @@
+import { Request, Response, NextFunction } from "express";
+import multer from "multer";
+
+// Centralized error handling function
+const ErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+  let errorMessage = err.message;
+
+  if (err instanceof multer.MulterError) {
+    const status =
+      err.code === "LIMIT_FILE_SIZE"
+        ? 413 // Payload Too Large
+        : err.code === "LIMIT_UNEXPECTED_FILE"
+        ? 400 // Bad Request
+        : 400; // default
+
+    res.status(status).json({
+      status: "Error",
+      code: err.code,
+      message: errorMessage,
+    });
+
+    return;
+  }
+
+  console.error(`Error: ${errorMessage}`);
+  // Send a response with a status code and error message
+  res.status(err.statusCode || 500).json({
+    status: "Error",
+    message: err.message || "Internal Server Error",
+    statusCode: err.statusCode || 500,
+  });
+};
+
+export default ErrorHandler;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,18 +1,46 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+import fs from "fs";
+import cors from "cors";
+import path from "path";
 import express from "express";
-import { SetupRabbitMQ } from "./config/rabbitmq-setup";
+import uploadRouter from "./routes/upload.routes";
+import ErrorHandler from "./middlewares/ErrorHandler";
 
+// Create upload directories if they don't exist
+const uploadDir = process.env.UPLOAD_DIR || "uploads";
+const originalDir = path.join(uploadDir, "original");
+const processedDir = path.join(uploadDir, "processed");
 
+const foldersArray = [uploadDir, originalDir, processedDir];
+
+foldersArray.forEach((dir) => {
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
+  }
+});
+
+// server and routes configurations
 const server = express();
 
 const PORT = process.env.PORT || 3000;
 
-const producer = new SetupRabbitMQ();
+server.use(cors());
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
 
-server.get("/", (req, res) => {
-  res.send("<h1>Server working</h1>");
+server.use("/api/upload", uploadRouter);
+
+server.get("/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
 });
 
+// handle all errors
+server.use(ErrorHandler);
+
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// Import and start the worker
+import "./workers/imageProcessor";
+
